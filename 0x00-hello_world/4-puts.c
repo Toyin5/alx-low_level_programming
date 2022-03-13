a68a6fced6b469dccc17cdc62bcbf062828bf88b
Toyin5 <toyinmuhammed50>
2022-03-12 20:00:13 -0800

    Added betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-/*
- * A function that uses puts
+/**
+ * main - output a text using puts
+ * Return: 0 if success
  *
  */
 int main(void)
 {
-    char str[55];
-    strcpy(str, "\"Programming is like building a multilingual puzzle");
-    puts(str);
-    return 0;
+	char str[55];
+	strcpy(str, " \"Programming is like building a multilingual puzzle");
+	puts(str);
+	return (0);
 }
-
